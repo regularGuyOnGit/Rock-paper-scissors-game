4427839b99c736f4f721a5e2179ab77e4d97558f
regularGuyOnGit <nitinrai3000@gmail.com>
2024-01-09 12:05:07 +0530

    fully functional ready

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const rockBtn = document.querySelector(".rock")
 const paperBtn = document.querySelector(".paper")
 const scissorsBtn = document.querySelector(".scissors")
 const resultDiv = document.querySelector(".result")
+const finalResult  = document.querySelector(".finalResult")
 
 // ? Just an array to store options of the game it will get selected randomly;
 const game_choices = ["Rock","Paper","Scissors","Rock","Paper","Scissors","Rock","Paper","Scissors","Rock","Paper"];
@@ -25,7 +26,7 @@ function gameRule(user_input,computer_choice){
         return("Computer Won");
     }
     else if( normalised_user_input === "rock" && normalised_computer_input ==="scissors"){
-        return("Player Won!");
+        return("Player Won");
     }
 
     else if (normalised_user_input === "paper" && normalised_computer_input==="rock"){
@@ -59,8 +60,82 @@ function displayResult(result){
 const resultText = document.createElement('h6');
 resultText.innerText = result
 resultDiv.appendChild(resultText);
-
 }
+
+
+let playerScore = 0
+let computerScore = 0
+let drawScore = 0
+
+
+function scoreBoard(scoreInDigit)  { 
+
+    if(scoreInDigit ==="Player Won"){
+     playerScore += 1
+
+    }
+    else if(scoreInDigit === "Computer Won"){
+        computerScore  +=  1  
+    }
+    else{
+         drawScore += 1
+    }
+}
+
+function gameResult(playerScore,computerScore,drawScore){
+    if(playerScore>computerScore){
+
+const divElement = document.createElement('div');
+const refreshBtn = document.createElement('button');
+
+divElement.innerText = "Player Won !"
+finalResult.appendChild(divElement);
+refreshBtn.addEventListener('click',()=>{
+    refreshPage()
+})
+refreshBtn.innerText = "Reload"
+finalResult.appendChild(refreshBtn);
+
+
+
+
+
+        // alert('Player Won!')
+        // refreshPage()
+    }
+    else if( computerScore>playerScore){
+ 
+const divElement = document.createElement('div');
+const refreshBtn = document.createElement('button');
+
+divElement.innerText = "Computer Won !"
+finalResult.appendChild(divElement);
+refreshBtn.addEventListener('click',()=>{
+    refreshPage()
+})
+refreshBtn.innerText = "Reload"
+finalResult.appendChild(refreshBtn);
+
+    }
+    else{
+
+        const divElement = document.createElement('div');
+        const refreshBtn = document.createElement('button');
+        
+        divElement.innerText = "Match Draw !"
+        finalResult.appendChild(divElement);
+        refreshBtn.addEventListener('click',()=>{
+            refreshPage()
+        })
+        refreshBtn.innerText = "Reload"
+        finalResult.appendChild(refreshBtn);
+
+    }
+}
+
+function refreshPage(){
+    window.location.reload();
+} 
 
 
 //! calling the function 
@@ -70,18 +145,44 @@ rockBtn.addEventListener('click',(e)=>{
     
    const resultOfGame= gameRule(e.target.innerText,computer_choice)
    displayResult(resultOfGame)
+   scoreBoard(resultOfGame)
+
+   if(playerScore + computerScore + drawScore ===5){
+    gameResult(playerScore,computerScore,drawScore)
+    // rockBtn.disabled = true;
+   }
+   
 })
+
 paperBtn.addEventListener('click',(e)=>{
     const computer_choice = game_choices[(Math.random().toFixed(1) * 10)];
     
    const resultOfGame= gameRule(e.target.innerText,computer_choice)
-   displayResult(resultOfGame)
+   displayResult(resultOfGame);
+   scoreBoard(resultOfGame)
+
+   if(playerScore + computerScore + drawScore ===5){
+    gameResult(playerScore,computerScore,drawScore)
+    // paperBtn.disabled = true;
+   }
 })
+
 scissorsBtn.addEventListener('click',(e)=>{
     // ? variable will store random option 
 const computer_choice = game_choices[(Math.random().toFixed(1) * 10)];
     
        const resultOfGame = gameRule(e.target.innerText,computer_choice)
        displayResult(resultOfGame)
+       scoreBoard(resultOfGame)
+
+       if(playerScore + computerScore + drawScore ===5){
+        gameResult(playerScore,computerScore,drawScore)
+        // scissorsBtn.disabled = true;
+       }
        
 })
+
+
+
+
+
